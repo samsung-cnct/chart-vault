b87898d80e20a64c05dfe46b68ab2d5d8540de47
Jim Conner <notjames@users.noreply.github.com>
2017-09-26 23:45:19 -0700

    Rebase merge and push (#11)
    * Initial commit -- tagging v0.8.3
    
    * Updated chart template
    
    * Fixed atlas link in README
    
    * Quick README edit
    
    * Chart template enhancements.
    
    * TEST: Jenkins still dying during build phase appearing to be that the setting of the CHART_VER and CHART_REL variables are not persisting through the command. I have seen this happen before when running the command the way the old method was so I am splitting the variable sets away from the envsubst command to see if this fixes the problem
    
    * adding .gitignore.
    
    * attempting to fix jenkinsfile issue where env variables for picking up VERSION and REL is failing during Jenkins build.
    
    * forget to add script to repo. adding now
    
    * fixed typo issue in script
    
    * Debugging script - tags still not getting picked up in repo
    
    * fixed minor bugs
    
    * Moved build_chart_tmpl.sh to build dir
    
    * adding .gitignore.
    
    * attempting to fix jenkinsfile issue where env variables for picking up VERSION and REL is failing during Jenkins build.
    
    * forget to add script to repo. adding now
    
    * fixed typo issue in script
    
    * Debugging script - tags still not getting picked up in repo
    
    * fixed minor bugs
    
    * Moved build_chart_tmpl.sh to build dir
    
    * fixed reference to build script in Jenkinsfile.
    
    * Found the problem with why the original Jenkinsfile was failing testing during build. It was a problem with the Jenkins repo environment missing a repo option ("advanced clone behaviors -- use all defaults"). Reverting to template Jenkinsfile
    
    * temporary for now (maybe) -- added back concept of a build script vs using kubesh to do the whole thing because Jenkins is still failing to persist the variables for chart release (CHART_REL) and chart version (CHART_VER) causing a yamllint to fail.
    
    * might have jumped too soon...checking. This effectively reverts my last commit....testing jenkins
    
    * OK. test failed for me. Will look into this tomorrow. For now, barring this build fails, I need to get this working and move on with the project.
    
    * Minor indent change to force a commit so I can tag it.
    
    * Changed chart name in jenkinsfile.
    
    * adding .gitignore.
    
    * attempting to fix jenkinsfile issue where env variables for picking up VERSION and REL is failing during Jenkins build.
    
    * forget to add script to repo. adding now
    
    * forget to add script to repo. adding now
    
    * fixed typo issue in script
    
    * fixed typo issue in script
    
    * Debugging script - tags still not getting picked up in repo
    
    * Debugging script - tags still not getting picked up in repo
    
    * fixed minor bugs
    
    * fixed minor bugs
    
    * Moved build_chart_tmpl.sh to build dir
    
    * Moved build_chart_tmpl.sh to build dir
    
    * fixed reference to build script in Jenkinsfile.
    
    * temporary removal of build-chart script for rebase
    
    * Found the problem with why the original Jenkinsfile was failing testing during build. It was a problem with the Jenkins repo environment missing a repo option ("advanced clone behaviors -- use all defaults"). Reverting to template Jenkinsfile
    
    * temporary for now (maybe) -- added back concept of a build script vs using kubesh to do the whole thing because Jenkins is still failing to persist the variables for chart release (CHART_REL) and chart version (CHART_VER) causing a yamllint to fail.
    
    * might have jumped too soon...checking. This effectively reverts my last commit....testing jenkins
    
    * OK. test failed for me. Will look into this tomorrow. For now, barring this build fails, I need to get this working and move on with the project.
    
    * Minor indent change to force a commit so I can tag it.
    
    * Changed chart name in jenkinsfile.

---
+++ build/build_chart_from_tmpl.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "The following tags exist: "
+git tag --list
+
+CHART_VER=$(git describe --tags --abbrev=0 | sed 's/^v//') 
+CHART_REL=$(git rev-list --count v${CHART_VER}..HEAD)
+
+envsubst < build/Chart.yaml.in > ${chart_name}/Chart.yaml
