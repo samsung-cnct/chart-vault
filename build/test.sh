68628f37d9caf1443766269e9adcbe79fa6550a2
Jim Conner <snafu.x@gmail.com>
2017-12-18 15:40:42 -0800

    Testing change to partial for environment variables. Apparently in an anchored script, variables are not properly interpolated. (#67)
    * Fixing deployment error from statefulset deployment where names etcdTls and vaultTls do not conform to dns1123 standards where the string must be lowercase.
    
    * Testing change to partial for environment variables. Apparently in an anchored script, variables are not properly interpolated. Trying out using k8s env variables.
    
    * yamllint dies on the colons in the env defs. Escaped them and trying again.
    
    * yamllint dies on the colons in the env defs still. This fixes it finally.

--- build/test.sh
+++ build/test.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 
-[[ -z "$CHART_NAME"   ]] && \
+[ -z $CHART_NAME   ] && \
   {
-    echo >&2 "Var '$CHART_NAME' is empty. Cannot continue."
+    echo >&2 "Var '\$CHART_NAME' is empty. Cannot continue."
     exit 1
   }
 
-[[ ! -d "${CHART_NAME}" ]] && \
+[ ! -d ${CHART_NAME} ] && \
   {
     echo >&2 "Directory for chart '$CHART_NAME' does not exist."
     exit 1
