88974d97513db0838b19761e5167a32c72f72b01
Marat G <maratoid@gmail.com>
2018-02-15 15:39:10 -0800

    add some smoke tests

--- test/cleanup.sh
+++ test/cleanup.sh
@@ -2,8 +2,9 @@
 
 # output any helm test logs if present
 kubectl get pod -o "custom-columns=NAME:.metadata.name" \
+  --show-all \
   -l "release=${RELEASE},app=smoke-test" \
-  -n "${NAMESPACE}" | tail -n +2 | while read line; do echo "${pod} Test Logs:"; echo; kubectl logs "${pod}" -n "${NAMESPACE}"; echo; done
+  -n ${NAMESPACE} | tail -n +2 | while read pod; do echo "${pod} Test Logs:"; echo; kubectl logs ${pod} -n ${NAMESPACE}; echo; done
 
 # clean up
 echo "Cleaning up"
